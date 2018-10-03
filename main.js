230cf88cd0da314cb5c8bdac188f2b02c793c56f
Sasha <sashakyunghwa@gmail.com>
2018-10-03 14:20:45 -0700

    calculate tip function and display properties

--- main.js
+++ main.js
@@ -16,3 +16,25 @@ function calculateTip() {
     } else {
       document.getElementById("each").style.display = "block";
     }
+
+//Calculate tip
+var total = (billAmt * serviceQual) / numOfPeople;
+//round to two decimal places
+total = Math.round(total * 100) / 100;
+//next line allows us to always have two digits after decimal point
+total = total.toFixed(2);
+//Display the tip
+document.getElementById("totalTip").style.display = "block";
+document.getElementById("tip").innerHTML = total;
+
+}
+
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click to call function
+document.getElementById("calculate").onclick = function() {
+calculateTip();
+
+};
